095fa577bb5ecd7eddf45ae6488f013e064cca03
Harrie Mayhew <hm@initials.co.uk>
2025-12-06 17:00:07 +0000

    create nuxt project

---
+++ nuxt.config.ts
@@ -0,0 +1,18 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
+
+export default defineNuxtConfig({
+  compatibilityDate: "2025-07-15",
+  devtools: { enabled: true },
+  css: ["./app/assets/main.css"],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  modules: ["@pinia/nuxt"],
+  runtimeConfig: {
+    public: {
+      supabaseUrl: process.env.SUPABASE_URL,
+      supabasePublishableKey: process.env.SUPABASE_PUBLISHABLE_KEY,
+    },
+  },
+});
