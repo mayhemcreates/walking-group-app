920992083c2285d1ec6f662cb9db2e3c2eb70cb1
Harrie Mayhew <hm@initials.co.uk>
2025-12-29 15:15:02 +0000

    add icons to nav

--- app/components/AppHeader.vue
+++ app/components/AppHeader.vue
@@ -41,15 +41,39 @@ const toggleNav = () => {
       </button>
       <nav :class="[mobNavIsOpen ? 'flex' : 'hidden']" class="fixed top-0 left-0 lg:static w-screen h-screen bg-teal-900 lg:bg-transparent lg:w-auto lg:h-auto lg:flex">
         <div class="flex justify-center w-full pt-20 lg:pt-0">
-          <ul class="flex flex-col lg:flex-row gap-4">
-            <li v-if="user"><NuxtLink @click.native="mobNavIsOpen = false" to="/schedule">Schedule</NuxtLink></li>
-            <li v-if="user"><NuxtLink @click.native="mobNavIsOpen = false" to="/createwalk">Add Walk</NuxtLink></li>
+          <ul class="flex flex-col lg:flex-row gap-6">
+            <li v-if="user">
+              <NuxtLink @click.native="mobNavIsOpen = false" to="/schedule" class="flex items-center gap-2">
+                <span>
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640" class="w-4 h-4">
+                    <path d="M216 64C229.3 64 240 74.7 240 88L240 128L400 128L400 88C400 74.7 410.7 64 424 64C437.3 64 448 74.7 448 88L448 128L480 128C515.3 128 544 156.7 544 192L544 480C544 515.3 515.3 544 480 544L160 544C124.7 544 96 515.3 96 480L96 192C96 156.7 124.7 128 160 128L192 128L192 88C192 74.7 202.7 64 216 64zM216 176L160 176C151.2 176 144 183.2 144 192L144 240L496 240L496 192C496 183.2 488.8 176 480 176L216 176zM144 288L144 480C144 488.8 151.2 496 160 496L480 496C488.8 496 496 488.8 496 480L496 288L144 288z" fill="white" />
+                  </svg> </span
+                >Schedule</NuxtLink
+              >
+            </li>
+            <li v-if="user">
+              <NuxtLink @click.native="mobNavIsOpen = false" to="/createwalk" class="flex items-center gap-2">
+                <span class="relative border-2 border-white rounded-full w-4 h-4 flex items-center justify-center">
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640" class="w-2 h-2">
+                    <path d="M352 128C352 110.3 337.7 96 320 96C302.3 96 288 110.3 288 128L288 288L128 288C110.3 288 96 302.3 96 320C96 337.7 110.3 352 128 352L288 352L288 512C288 529.7 302.3 544 320 544C337.7 544 352 529.7 352 512L352 352L512 352C529.7 352 544 337.7 544 320C544 302.3 529.7 288 512 288L352 288L352 128z" fill="white" />
+                  </svg>
+                </span>
+                Add Walk</NuxtLink
+              >
+            </li>
             <li v-if="!user"><NuxtLink @click.native="mobNavIsOpen = false" to="/login">Login</NuxtLink></li>
-            <li v-if="user"><button class="cursor-pointer" @click="logout">Logout</button></li>
+            <li v-if="user">
+              <button class="cursor-pointer flex items-center gap-2" @click="logout">
+                <span>
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640" class="w-4 h-4">
+                    <path d="M224 160C241.7 160 256 145.7 256 128C256 110.3 241.7 96 224 96L160 96C107 96 64 139 64 192L64 448C64 501 107 544 160 544L224 544C241.7 544 256 529.7 256 512C256 494.3 241.7 480 224 480L160 480C142.3 480 128 465.7 128 448L128 192C128 174.3 142.3 160 160 160L224 160zM566.6 342.6C579.1 330.1 579.1 309.8 566.6 297.3L438.6 169.3C426.1 156.8 405.8 156.8 393.3 169.3C380.8 181.8 380.8 202.1 393.3 214.6L466.7 288L256 288C238.3 288 224 302.3 224 320C224 337.7 238.3 352 256 352L466.7 352L393.3 425.4C380.8 437.9 380.8 458.2 393.3 470.7C405.8 483.2 426.1 483.2 438.6 470.7L566.6 342.7z" fill="white"/>
+                  </svg> </span
+                >Logout
+              </button>
+            </li>
           </ul>
         </div>
       </nav>
     </div>
   </header>
 </template>
-
